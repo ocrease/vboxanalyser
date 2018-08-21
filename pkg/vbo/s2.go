49d7ef22d7b61c4429a38d4127cb978611555004
Oliver Crease <oliver.crease@gmail.com>
2018-08-21 21:51:14 +0100

    Improve accuracy of lap timing

--- pkg/vbo/s2.go
+++ pkg/vbo/s2.go
@@ -14,7 +14,7 @@ const (
 var gateWidth = s1.ChordAngleFromAngle(s1.Angle(12.5 / earthRadius))
 
 func processLaps(f File) (laps []Lap) {
-	startLine := s2.LatLngFromDegrees(f.Start.Lat, f.Start.Lng)
+	startLine := s2.PointFromLatLng(s2.LatLngFromDegrees(f.Start.Lat, f.Start.Lng))
 	rows := f.Data.Rows
 
 	latIndex := f.Columns["lat"]
@@ -24,11 +24,30 @@ func processLaps(f File) (laps []Lap) {
 		s1Lng := r.GetValue(longIndex).(float64)
 		return s2.LatLngFromDegrees(s1Lat/60, s1Lng*-1/60)
 	}
+	startFraction := func(i int) time.Duration {
+		if i <= 1 || i == len(rows)-1 {
+			return 0
+		}
+		a := s2.PointFromLatLng(latLng(rows[i-1]))
+		b := s2.PointFromLatLng(latLng(rows[i]))
+		x := s2.Project(startLine, a, b)
+		sf := s2.DistanceFraction(x, a, b)
+		sf = 1.0 - sf
+
+		duration := float64(f.duration(i-1, i)) * sf
+		return time.Duration(duration)
+	}
+
+	duration := func(s int, e int) time.Duration {
+		sd := startFraction(s)
+		ed := startFraction(e)
+		return f.duration(s, e) + sd - ed
+	}
 
 	start := 0
 	end := 0
-	var s s2.LatLng
-	var e s2.LatLng
+	var s s2.Point
+	var e s2.Point
 	minDistance := gateWidth
 	partial := true
 	firstLap := true
@@ -37,10 +56,10 @@ func processLaps(f File) (laps []Lap) {
 	nearStartLine := false
 	for i, v := range rows {
 		if i == 0 {
-			e = latLng(v)
+			e = s2.PointFromLatLng(latLng(v))
 			comp := s2.CompareDistance(
-				s2.PointFromLatLng(startLine),
-				s2.PointFromLatLng(e),
+				startLine,
+				e,
 				gateWidth)
 			if comp < 1 {
 				inRange = true
@@ -51,17 +70,17 @@ func processLaps(f File) (laps []Lap) {
 			continue
 		}
 		s = e
-		e = latLng(v)
+		e = s2.PointFromLatLng(latLng(v))
 		inRange = s2.IsDistanceLess(
-			s2.PointFromLatLng(startLine),
-			s2.PointFromLatLng(s),
-			s2.PointFromLatLng(e),
+			startLine,
+			s,
+			e,
 			gateWidth)
 		if inRange {
 			d, updated := s2.UpdateMinDistance(
-				s2.PointFromLatLng(startLine),
-				s2.PointFromLatLng(s),
-				s2.PointFromLatLng(e),
+				startLine,
+				s,
+				e,
 				minDistance)
 			if updated {
 				end = i
@@ -73,7 +92,7 @@ func processLaps(f File) (laps []Lap) {
 			if firstLap {
 				partial = !startedInRange
 			}
-			laps = append(laps, newLap(start, end, partial, f.duration(start, end)))
+			laps = append(laps, newLap(f, start, end, partial, duration))
 			start = end
 			partial = false
 			firstLap = false
@@ -85,17 +104,17 @@ func processLaps(f File) (laps []Lap) {
 		if firstLap {
 			partial = !startedInRange
 		}
-		laps = append(laps, newLap(start, end, partial, f.duration(start, end)))
+		laps = append(laps, newLap(f, start, end, partial, duration))
 		start = end
 	}
 	if start != (len(rows) - 1) {
-		laps = append(laps, newLap(start, len(rows)-1, true, f.duration(start, len(rows)-1)))
+		laps = append(laps, newLap(f, start, len(rows)-1, true, duration))
 	}
 	return
 }
 
-func newLap(s int, e int, partial bool, lapTime time.Duration) Lap {
-	return Lap{startIndex: s, endIndex: e, Partial: partial, LapTime: jsonDuration(lapTime)}
+func newLap(f File, s int, e int, partial bool, duration func(a, b int) time.Duration) Lap {
+	return Lap{startIndex: s, endIndex: e, Partial: partial, LapTime: jsonDuration(duration(s, e).Round(10 * time.Millisecond))}
 }
 
 // func Distance(f *vbo.File) float64 {
